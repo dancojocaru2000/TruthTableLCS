797afa18d415299e538f7349292672caaa73db2c
Dan Cojocaru <dancojocaru2010@hotmail.com>
2020-11-09 13:41:49 +0200

    Removed "is implied by" operator from program output as it was not implemented

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from logical_prop import LogicProposition, gen_table, get_max_width, print_table
 
-print(" ! is the symbol for not\n","| is the symbol for or\n","& is the symbol for and\n","> is the symbol for implies\n","< is the symbol for is implied by\n","= is the symbol for if and only if\n","Any upper case letter is an atomic proposition")
+print(" ! is the symbol for not\n","| is the symbol for or\n","& is the symbol for and\n","> is the symbol for implies\n","= is the symbol for if and only if\n","Any upper case letter is an atomic proposition")
 p = input(" Plese input a string of symbols:\n")
 
 prop = LogicProposition(p)
