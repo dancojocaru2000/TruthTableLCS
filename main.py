2e393059d39e53782e5ff9d8c8eadb3430a3b6ad
Dan Cojocaru <dancojocaru2010@hotmail.com>
2020-11-10 21:07:45 +0200

    Correct printing when stdout is piped/not a terminal

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 from logical_prop import LogicProposition, gen_table, get_max_width, print_table
-from sys import stdin
+from sys import stdout
 
 p = None
-if stdin.isatty():
+if stdout.isatty():
 	print(" ! is the symbol for not\n","| is the symbol for or\n","& is the symbol for and\n","> is the symbol for implies\n","= is the symbol for if and only if\n","Any upper case letter is an atomic proposition")
 	p = input(" Please input a string of symbols:\n")
 else:
@@ -13,7 +13,6 @@ else:
 prop = LogicProposition(p)
 
 def should_print_color():
-	from sys import stdout
 	# Print color only if the output is a terminal
 	# If the output is a file, print normally
 	if not stdout.isatty():
