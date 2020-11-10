cd6778b439b993372a3efe9031f53b7eb7516945
Dan Cojocaru <dancojocaru2010@hotmail.com>
2020-11-10 23:00:13 +0200

    Considered both stdin and stdout not to be piped in order to print

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 from logical_prop import LogicProposition, gen_table, get_max_width, print_table
-from sys import stdout
+from sys import stdout, stdin
 
 p = None
-if stdout.isatty():
+if stdout.isatty() and stdin.isatty():
 	print(" ! is the symbol for not\n","| is the symbol for or\n","& is the symbol for and\n","> is the symbol for implies\n","= is the symbol for if and only if\n","Any upper case letter is an atomic proposition")
 	p = input(" Please input a string of symbols:\n")
 else:
